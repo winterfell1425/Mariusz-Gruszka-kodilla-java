36ac982487623f89d5c5e968bcc2a0533c9c5b71
winterfell1425 <winterfell1425@gmail.com>
2018-10-22 13:44:27 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        Calculator calculator1 = new Calculator();
+        int result1 = calculator1.add(5,2);
+        if (result1 == 7) {
+            System.out.println("Result = " + result1 + "; test OK.");
+        } else {
+            System.out.println("Error!");
+        }
+        Calculator calculator2 = new Calculator();
+        int result2 = calculator2.subtract(5,2);
+        if (result2 == 3) {
+            System.out.println("Result = " + result2 + "; test OK.");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
