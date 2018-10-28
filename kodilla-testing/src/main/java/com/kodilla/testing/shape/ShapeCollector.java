5d6512de3616da6a59497bd209969de94f3a7b6e
winterfell1425 <winterfell1425@gmail.com>
2018-10-28 21:46:14 +0100

    6_4_mod

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -7,12 +7,15 @@ public class ShapeCollector {
     ArrayList<Shape> myShapes = new ArrayList<>();
 
     void addFigure(Shape shape) {
+
         myShapes.add(shape);
     }
     boolean removeFigure(Shape shape){
-        boolean result;
-        myShapes.remove(shape);
-        result = true;
+        boolean result = false;
+        if (myShapes.contains(shape)) {
+            myShapes.remove(shape);
+            result = true;
+        }
         return result;
     }
 
