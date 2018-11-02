a22fb4b9798b04d1b1482b9b7b4074cc851c3f63
winterfell1425 <winterfell1425@gmail.com>
2018-11-02 10:03:41 +0100

    kodilla 7_2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -10,5 +11,8 @@ public class StreamMain {
         poemBeautifier.beautify("red rose???",(poem) -> "ABC" + poem + "ABC");
         poemBeautifier.beautify("YELLOW ROSE....",(poem) -> poem.toLowerCase());
         poemBeautifier.beautify("White, white Rose!;%@,,;",(poem) -> poem.substring(7, 17));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
