ea13f7bb819b81b9038551914892440d5dff7dd0
winterfell1425 <winterfell1425@gmail.com>
2018-11-05 21:58:36 +0100

    error

--- kodilla-stream/src/main/java/com/kodilla/stream/book/BookDirectory.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/book/BookDirectory.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public final class BookDirectory {
     private final List<Book> theBookList = new ArrayList<>();
-
     public BookDirectory() {
         theBookList.add(new Book("Dylan Murphy", "Wolf of the mountain",
                 2003, "0001"));
