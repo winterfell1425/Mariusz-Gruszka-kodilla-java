bcfc48623f521eb96e848c25aaf6ceda8ca417ec
winterfell1425 <winterfell1425@gmail.com>
2018-11-08 18:02:05 +0100

    kodilla_8_2

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,29 @@
+package com.kodilla.exception.test;
+
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        try {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        double result = firstChallenge.divide(3, 0);
+        System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("You've used zero as a divisor!!!");
+        } finally {
+            System.out.println("End of first challenge in division.");
+        }
+    }
+}
