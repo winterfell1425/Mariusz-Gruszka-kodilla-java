a6fc5176852c211a8abc2aba5b33dd39c8bb1097
winterfell1425 <winterfell1425@gmail.com>
2018-11-29 17:28:50 +0100

    kodilla_11_1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+    @Test
+    public void testGetLastLog() {
+        //Given
+        Logger.getInstance().log("bbbb");
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals("bbbb", lastLog);
+    }
+}
